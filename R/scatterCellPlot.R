4dbdbea1e5509e235acb78d2ed072237eddedda8
JunJunLao <1138976957@qq.com>
2023-08-29 16:08:29 +0800

    bugs fixed

--- R/scatterCellPlot.R
+++ R/scatterCellPlot.R
@@ -41,6 +41,19 @@ scatterCellPlot <- function(object = NULL,
   pc12 <- cbind(reduc, meta)
   pc12$idents <- Seurat::Idents(object)
 
+  # summary celltype numbers
+  if(is.null(cell.id)){
+    cell_num <- pc12 |>
+      dplyr::group_by(idents) |>
+      dplyr::summarise(n = dplyr::n()) |>
+      dplyr::arrange(n)
+  }else{
+    cell_num <- pc12 |>
+      dplyr::group_by(idents,.data[[cell.id]]) |>
+      dplyr::summarise(n = dplyr::n()) |>
+      dplyr::arrange(n)
+  }
+
   # ============================================================================
   # 2_draw plot
   # ============================================================================
@@ -60,11 +73,13 @@ scatterCellPlot <- function(object = NULL,
   ))
   grid.rect()
   if(rm.axis == FALSE){
-    grid.xaxis()
-    grid.yaxis()
+    # grid.xaxis()
+    # grid.yaxis()
+    jjPlot::grid.xaxis2(label.space = 0.5)
+    jjPlot::grid.yaxis2(label.space = 0.25)
   }
 
-  celltype <- unique(pc12$idents)
+  celltype <- cell_num$idents
 
   if(is.null(color)){
     # create colors
@@ -104,20 +119,6 @@ scatterCellPlot <- function(object = NULL,
 
   # ============================================================================
   # barplot
-  # summary celltype numbers
-  if(is.null(cell.id)){
-    cell_num <- pc12 |>
-      dplyr::group_by(idents) |>
-      dplyr::summarise(n = dplyr::n()) |>
-      dplyr::arrange(n)
-  }else{
-    cell_num <- pc12 |>
-      dplyr::group_by(idents,.data[[cell.id]]) |>
-      dplyr::summarise(n = dplyr::n()) |>
-      dplyr::arrange(n)
-  }
-
-
   pushViewport(viewport(x = unit(0.61, "npc"), y = unit(0.5, "npc"),
                         width = unit(bar.width, "npc"),
                         height = unit(0.7, "npc"),
@@ -126,7 +127,10 @@ scatterCellPlot <- function(object = NULL,
                         xscale = c(0,max(cell_num$n) + 0.1*max(cell_num$n))))
 
   if(rm.axis == FALSE){
-    grid.xaxis()
+    # grid.xaxis()
+    jjPlot::grid.xaxis2(label.space = 0.5,
+                        at = c(0,max(cell_num$n)),
+                        labels = as.character(c(0,max(cell_num$n))))
   }
   grid.rect(x = rep(0,nrow(cell_num)),y = 1:nrow(cell_num),
             width = cell_num$n,height = unit(0.08,"npc"),
